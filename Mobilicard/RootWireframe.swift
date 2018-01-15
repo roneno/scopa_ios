6adc26a8db33c85dba7ff83ce77571af80f02f82
Vsevolod Ban <vsevolod@vsevolods-macbook-pro.local>
2018-01-15 12:59:11 +0300

    Created Architecture, Added Screen Preparations(Home, Login, About, Payment Method, Tech Support, Scanning, Privacy Policy), Added Root Wireframe.

---
+++ Mobilicard/RootWireframe.swift
@@ -0,0 +1,30 @@
+//
+//  RootWireframe.swift
+//  Mobilicard
+//
+//  Created by Vsevolod on 1/15/18.
+//  Copyright © 2018 Vsevolod. All rights reserved.
+//
+
+import UIKit
+
+class RootWireframe {
+    
+//    private let loginScreenWireframe: LoginWireframe
+//
+//    init() {
+//        self.loginScreenWireframe = LoginWireframe.sharedInstance
+//    }
+//
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?, window: UIWindow) -> Bool {
+//        self.loginScreenWireframe.window = window
+//        self.checkIfUserLoggedOut()
+//        return true
+//    }
+//
+//    func checkIfUserLoggedOut(){
+//
+//        self.loginScreenWireframe.presentLoginScreenViewControllerInWindow()
+//    }
+}
+
